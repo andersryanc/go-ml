6e87e6ea0573dcceb847f35fb31b31bfcb714dd2
Ryan Anderson <anderson.ryanc@gmail.com>
2020-07-30 01:36:40 -0700

    use gradient descent for better results

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"image/color"
 	"log"
-	"math"
 	"os"
 
 	"gonum.org/v1/plot"
@@ -13,7 +13,20 @@ import (
 	"gonum.org/v1/plot/vg/draw"
 )
 
+// Use bash to run this with an increasing numebr of iterations:
+// ```
+// for n in $(seq 0 100)
+// do
+//   go run main.go -n $n; sleep 1
+// done
+// ```
+
+var iterations int
+
 func main() {
+	flag.IntVar(&iterations, "n", 1000, "number of iterations")
+	flag.Parse()
+
 	xys, err := readData("data.txt")
 	if err != nil {
 		log.Fatalf("could not read data.txt: %v", err)
@@ -76,7 +89,7 @@ func plotData(path string, xys plotter.XYs) error {
 	p.Add(s)
 
 	// create linear regression result
-	m, c := linearRegression(xys)
+	m, c := linearRegression(xys, 0.01)
 	l, err := plotter.NewLine(plotter.XYs{
 		{X: 3, Y: (3 * m) + c}, {X: 20, Y: (20 * m) + c},
 	})
@@ -102,35 +115,63 @@ func plotData(path string, xys plotter.XYs) error {
 	return nil
 }
 
-func linearRegression(xys plotter.XYs) (m, c float64) {
-	const (
-		min   = -100.0
-		max   = 100.0
-		delta = 0.1
-	)
+func linearRegression(xys plotter.XYs, alpha float64) (m, c float64) {
+	// NOTE: This is a simple way of finding the line of best fit
+	// ...BUT, it takes 4004001 iterations
+	// const (
+	// 	min   = -100.0
+	// 	max   = 100.0
+	// 	delta = 0.1
+	// )
+	// count := 0
+	// minCost := math.MaxFloat64
+	// for im := min; im < max; im += delta {
+	// 	for ic := min; ic < max; ic += delta {
+	// 		count++
+	// 		cost := computeCost(xys, im, ic)
+	// 		if cost < minCost {
+	// 			minCost = cost
+	// 			m, c = im, ic
+	// 			dm, dc := computeGradient(xys, m, c)
+	// 			fmt.Printf("grad(%.2f, %.2f) = (%.2f, %.2f)\n", m, c, dm, dc)
+	// 		}
+	// 	}
+	// }
+	// fmt.Printf("cost(%.2f, %.2f) = %.2f\n", m, c, computeCost(xys, m, c))
+	// fmt.Printf("tried %d times\n", count)
 
-	minCost := math.MaxFloat64
-	for im := min; im < max; im += delta {
-		for ic := min; ic < max; ic += delta {
-			cost := computeCost(xys, im, ic)
-			if cost < minCost {
-				minCost = cost
-				m, c = im, ic
-			}
-		}
+	// NOTE: Instead, lets use gradient descent:
+	for i := 0; i < iterations; i++ {
+		dm, dc := computeGradient(xys, m, c)
+		m += -dm * alpha
+		c += -dc * alpha
+		// fmt.Printf("grad(%.2f, %.2f) = (%.2f, %.2f)\n", m, c, dm, dc)
 	}
-
 	fmt.Printf("cost(%.2f, %.2f) = %.2f\n", m, c, computeCost(xys, m, c))
 
 	return m, c
 }
 
 func computeCost(xys plotter.XYs, m, c float64) float64 {
-	// cost = 1/N sum((y - (m*x+c))^2)
+	// cost = 1/N * sum((y - (m*x+c))^2)
 	s := 0.0
 	for _, xy := range xys {
 		d := xy.Y - (xy.X*m + c)
 		s += d * d
 	}
 	return s / float64(len(xys))
+}
+
+func computeGradient(xys plotter.XYs, m, c float64) (dm, dc float64) {
+	// cost = 1/N sum((y - (m*x+c))^2)
+	// d = (y - (m*x+c))
+	// cost/dm = 2/N * sum(-x * d)
+	// cost/dc = 2/N * sum(-d)
+	for _, xy := range xys {
+		d := xy.Y - (xy.X*m + c)
+		dm += -xy.X * d
+		dc += -d
+	}
+	n := float64(len(xys))
+	return 2 / n * dm, 2 / n * dc
 }
